8d8c544d9b4941816ff5fd28de3a6ffe3a1a7436
xaviguasch <xaviguasch@gmail.com>
2020-02-12 11:03:15 +0100

    implemented play and stop functionality

--- script.js
+++ script.js
@@ -6,12 +6,20 @@ const timestamp = document.querySelector('#timestamp')
 
 // Play and pause video
 function toggleVideoStatus() {
-  return true
+  if (video.paused) {
+    video.play()
+  } else {
+    video.pause()
+  }
 }
 
 // Update play/pause icon
 function updatePlayIcon() {
-  return true
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>'
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>'
+  }
 }
 
 // Update progress and timestamp
@@ -26,14 +34,15 @@ function setVideoProgress() {
 
 // Stop video
 function stopVideo() {
-  return true
+  video.currentTime = 0
+  video.pause()
 }
 
 // Event listeners
 
 video.addEventListener('click', toggleVideoStatus)
 video.addEventListener('pause', updatePlayIcon)
-video.addEventListener('play', updateProgress)
+video.addEventListener('play', updatePlayIcon)
 video.addEventListener('timeupdate', updateProgress)
 
 play.addEventListener('click', toggleVideoStatus)
